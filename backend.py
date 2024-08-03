0bd7628eb58e778b61fd3cc602c1d42acd7964b8
Jay Ravalani <jay.ravalani746@gmail.com>
2024-08-03 17:08:38 +0530

    implement line graph and sky conditions

--- backend.py
+++ backend.py
@@ -7,18 +7,13 @@ load_dotenv()
 api_key = os.getenv('API_KEY')
 
 
-def get_data(place, forecast_days, type):
+def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     total_values = 8 * forecast_days
     filtered_data = filtered_data[:total_values]
-    match type:
-        case "Temperature":
-            filtered_data = [temperatures['main']['temp'] / 10 for temperatures in filtered_data]
-        case "Sky":
-            filtered_data = [skys['weather'][0]['main'] for skys in filtered_data]
     return filtered_data
 
 
--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 import streamlit as st
 import plotly.express as px
-from backend import get_data
+import backend
+import pandas as pd
 
-st.header("Weather Forecast for the Next Days")
+st.header("Weather Forecast")
 
 place = st.text_input("Place: ")
+place = place.capitalize()
+forecast_days = st.slider("Forecast Days ", min_value=1, max_value=5, help="Select number of days")
+kind = st.selectbox("Select data to view", options=['Temperature', 'Sky'])
 
-days = st.slider("Forecast Days ", min_value=0, max_value=5, help="Select number of days")
 
-option = st.selectbox("Select data to view", options=['Temperature', 'Sky'])
+st.subheader(f"{kind} for the next {forecast_days} day(s) in {place}")
 
-st.subheader(f"Temperature for the next {days} day(s) in {place}")
+if place:
+    try:
+        # Get the temperature/sky
+        filtered_data= backend.get_data(place, forecast_days)
 
-st.plotly_chart()
+        # Get the data and create the chart
+        match kind:
+            case "Temperature":
+                temperatures = [dict['main']['temp'] / 10 for dict in filtered_data]
+                dates = [dict['dt_txt'] for dict in filtered_data]
+                df = pd.DataFrame({"Date": dates, "Temperature (C)": temperatures})
+                figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+                st.plotly_chart(figure)
+
+            case "Sky":
+                images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                          "Rain": "images/rain.png", "Snow": "images/snow.png"}
+                sky_conditions = [skys['weather'][0]['main'] for skys in filtered_data]
+                image_path = [images[condition] for condition in sky_conditions]
+                st.image(image_path, width=115)
+    except KeyError:
+        st.info("Enter a valid city name.")
