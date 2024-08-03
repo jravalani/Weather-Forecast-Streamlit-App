d442ad1a7b0a0f91045583abd4580d47ece0b682
Jay Ravalani <jay.ravalani746@gmail.com>
2024-08-03 15:30:25 +0530

    extracted temperature and cloud condition from api

--- backend.py
+++ backend.py
@@ -6,12 +6,21 @@ load_dotenv()
 
 api_key = os.getenv('API_KEY')
 
-def get_data(place):
+
+def get_data(place, forecast_days, type):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
-    content = response.json()
-    return content
+    data = response.json()
+    filtered_data = data["list"]
+    total_values = 8 * forecast_days
+    filtered_data = filtered_data[:total_values]
+    match type:
+        case "Temperature":
+            filtered_data = [temperatures['main']['temp'] / 10 for temperatures in filtered_data]
+        case "Sky":
+            filtered_data = [skys['weather'][0]['main'] for skys in filtered_data]
+    return filtered_data
 
 
 if __name__ == "__main__":
-    print(get_data(place="Tokyo"))
+    print(get_data(place="Tokyo", forecast_days=3, type="Sky"))
